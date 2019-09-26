f32b16fa6ade30bbb115d5076ddae2b912cc72f6
Tim Weber <scy@scy.name>
2019-09-26 18:08:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ewhome
+
+## Config schreiben
+
+Unter `config` gibt es das Script `push-config-to-mqtt.py`, das die Konfiguration aus `ewhome.yaml` in JSON konvertiert und in MQTT ablegt.
+
+Beispielaufruf:
+
+```sh
+cd config
+pipenv run python push-config-to-mqtt.py
+```
---
+++ config/push-config-to-mqtt.py
@@ -0,0 +1,51 @@
+from json import dumps
+import paho.mqtt.client as paho
+import time
+
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+config_file = "ewhome.yaml"
+broker_address = "10.0.0.88"
+topic = "ewh/config"
+published = False
+
+
+def read_yaml_file(filename):
+    with open(filename, "r") as file:
+        config = load(file, Loader=Loader)
+    return config
+
+def to_json(config):
+    return dumps(config)
+
+
+def on_connect(client, userdata, flags, rc):
+    client.publish(topic, data, qos=1, retain=True)
+
+def on_publish(client, userdata, result):
+    global published
+    published = True
+    client.disconnect()
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+def connect_and_push():
+    mqtt = paho.Client()
+    mqtt.on_connect = on_connect
+    mqtt.on_publish = on_publish
+    mqtt.on_log = on_log
+    mqtt.connect(broker_address)
+    mqtt.loop_start()
+
+
+if __name__ == "__main__":
+    data = to_json(read_yaml_file(config_file))
+    connect_and_push()
+    while not published:
+        time.sleep(0.1)
