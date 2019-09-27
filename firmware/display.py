a2cb903cca4cd7db70331e755ef8a199b151c2bb
Tim Weber <mail@timweber.name>
2019-09-27 16:12:31 +0200

    Add display.text()

--- firmware/display.py
+++ firmware/display.py
@@ -6,6 +6,7 @@ class Display(Task):
         super().__init__()
         self.driver = driver
         self.mac = "..."
+        self.lines = [""] * 6
         self.network = None
         self.mqtt = None
         self.blip = True
@@ -19,9 +20,18 @@ class Display(Task):
     def set_mqtt(self, mqtt):
         self.mqtt = mqtt
 
+    def text(self, text, line):
+        if line > 5:
+            raise RuntimeError("You may only access lines 0 to 5.")
+        self.lines[line] = text
+
     def update(self, scheduler):
         # Clear display.
         self.driver.fill(0)
+
+        # User-defined lines.
+        for row in range(6):
+            self.driver.text(self.lines[row], 0, 8 * row)
 
         # Status bar.
         self.driver.text(self.mac, 0, 49)
