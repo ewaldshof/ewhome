790553e953d211de6213a240e6029adcd27d7cb7
Tim Weber <mail@timweber.name>
2019-09-27 16:11:27 +0200

    Replace config.get_mine() with config.mine

--- firmware/config.py
+++ firmware/config.py
@@ -5,6 +5,7 @@ class Config:
     def __init__(self, network, mqtt):
         self.mac = network.mac
         self.data = {}
+        self.mine = None
         self.listeners = []
         mqtt.subscribe("ewh/config", self.on_mqtt)
 
@@ -13,11 +14,9 @@ class Config:
             self.data = ujson.loads(msg)
         except:
             print("JSON config decode error: " + msg)
+        self.mine = self.data.get("esps", {}).get(self.mac, None)
         for listener in self.listeners:
             listener(self)
 
     def on_update(self, listener):
         self.listeners.append(listener)
-
-    def get_mine(self):
-        return self.data.get("esps", {}).get(self.mac, None)
