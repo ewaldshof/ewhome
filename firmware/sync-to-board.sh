d5d39f23ccbb9048a0c1feb80f6b3a24acda9969
Stephan Waldtmann <stephan@waldtmann.de>
2019-10-20 17:06:03 +0200

    Testing with ampy.

--- firmware/sync-to-board.sh
+++ firmware/sync-to-board.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+export AMPY_DELAY=0.5
+export AMPY_PORT=/dev/tty.SLAB_USBtoUART
+
+filelist="heating.py
+task.py
+drivers/ssd1306.py
+drivers/__init__.py
+config.py
+board/bohei.py
+board/__init__.py
+board/base.py
+boot.py
+ewh_net.py
+mqtt.py
+temperature.py"
+
+
 echo "Creating directories ..."
 for dir in $(find . -type d | cut -c 3-); do
     if [ "$dir" = '' ]; then
@@ -11,6 +28,7 @@ done
 
 echo "Copying files ..."
 for file in $(find . -name '*.py' | cut -c 3-); do
+#for file in $filelist; do
     echo "  $file"
     ampy put "$file" "$file"
 done
