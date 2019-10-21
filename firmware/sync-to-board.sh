527714bf0880bb5d9756a92d11d004ff8e8519d0
Stephan Waldtmann <stephan@waldtmann.de>
2019-10-21 12:31:48 +0200

    Cleanup

--- firmware/sync-to-board.sh
+++ firmware/sync-to-board.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
 
-export AMPY_DELAY=0.5
+#set -x
 
 PORT=${1:-"/dev/tty.SLAB_USBtoUART"}
 
-export AMPY_PORT=$PORT
-
-CMD=${2:-"rshell --port $PORT"}
-
-filelist="heating.py
-task.py
-drivers/ssd1306.py
-drivers/__init__.py
-config.py
-board/bohei.py
-board/__init__.py
-board/base.py
-boot.py
-ewh_net.py
-mqtt.py
-temperature.py"
-
+QUIET="--quiet"
 
 echo "Creating directories ..."
 for dir in $(find . -type d | cut -c 3-); do
@@ -28,12 +12,20 @@ for dir in $(find . -type d | cut -c 3-); do
         continue
     fi
     echo "  $dir"
-    rshell --port $PORT cp -r "$dir" "/pyboard/"
+    rshell $QUIET --port $PORT cp -r "$dir" "/pyboard/" && {
+        echo "Done."
+    } || {
+        echo "rshell failed!"
+        exit 1
+    }
 done
 
 echo "Copying files ..."
-#for file in $(find . -name '*.py' | cut -c 3-); do
-#for file in $filelist; do
 echo "  *.py files"
-rshell --port $PORT cp "*.py" "/pyboard/"
-#done
+rshell $QUIET --port $PORT cp "*.py" "/pyboard/" && {
+    echo "Done."
+} || {
+    echo "rshell failed!"
+    exit 1
+}
+
