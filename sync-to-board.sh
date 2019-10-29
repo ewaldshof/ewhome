5019cdf2a241c5cfc3afe6918cea110bfe7e066a
Tim Weber <mail@timweber.name>
2019-10-29 14:57:12 +0100

    Indent sync-to-board.sh with tabs

--- sync-to-board.sh
+++ sync-to-board.sh
@@ -11,23 +11,23 @@ QUIET="--quiet"
 
 echo "Creating directories ..."
 for dir in $(find . -type d | cut -c 3-); do
-    if [ "$dir" = '' ]; then
-        continue
-    fi
-    echo "  $dir"
-    rshell $QUIET --port $PORT cp -r "$dir" "/pyboard/" && {
-        echo "Done."
-    } || {
-        echo "rshell failed!"
-        exit 1
-    }
+	if [ "$dir" = '' ]; then
+		continue
+	fi
+	echo "  $dir"
+	rshell $QUIET --port $PORT cp -r "$dir" "/pyboard/" && {
+		echo "Done."
+	} || {
+		echo "rshell failed!"
+		exit 1
+	}
 done
 
 echo "Copying files ..."
 echo "  *.py files"
 rshell $QUIET --port $PORT cp "*.py" "/pyboard/" && {
-    echo "Done."
+	echo "Done."
 } || {
-    echo "rshell failed!"
-    exit 1
+	echo "rshell failed!"
+	exit 1
 }
