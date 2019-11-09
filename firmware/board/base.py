be60d6aaea1a59dea7ca1b7044dedba82f0041f0
Tim Weber <mail@timweber.name>
2019-11-09 15:22:46 +0100

    Add pin management to board

--- firmware/board/base.py
+++ firmware/board/base.py
@@ -1,6 +1,6 @@
 from display import Display
 import drivers.ssd1306 as ssd1306
-from machine import I2C
+from machine import I2C, Pin
 from temperature import Temperature
 
 class Board():
@@ -10,6 +10,8 @@ class Board():
         self.mqtt = mqtt
         self.display = None      # Both will need to be initialized by the actual board, at least with a noop class,
         self.temperature = None  # because the current code relies on these being set.
+        self.pins_by_num = {}
+        self.pins_by_name = {}
 
     def init_ds18x20(self, ow_pin):
         print("Initializing DS18X20.")
@@ -37,3 +39,24 @@ class Board():
             print("SSD1306 initialized.")
         except OSError as err:
            print("OS error: {0}".format(err))
+
+    def init_pin(self, number, name, mode=-1, pull=None):
+        number = int(number)
+        name = str(name)
+        if number in self.pins_by_num:
+            raise RuntimeError(
+                "attempted second initialization of pin {0} as {1}; do you have a pin conflict?".format(number, name)
+            )
+        if name in self.pins_by_name:
+            raise RuntimeError("pin name {0} already in use".format(name))
+        pin = Pin(number, mode, pull)
+        self.pins_by_num[number] = pin
+        self.pins_by_name[name] = pin
+        return pin
+
+    def get_pin(self, id):
+        if type(id) is int:
+            return self.pins_by_num[id]
+        if type(id) is str:
+            return self.pins_by_name[id]
+        raise TypeError("id has to be int or str, not {0}".format(type(id)))
--- firmware/board/bohei.py
+++ firmware/board/bohei.py
@@ -6,20 +6,21 @@ class Board(BaseBoard):
 
     def init(self):
         self.init_ssd1306i2c(
-            reset_pin=Pin(16, Pin.OUT),
-            scl_pin=Pin(15),
-            sda_pin=Pin(4),
+            reset_pin=self.init_pin(16, "Display Reset", Pin.OUT),
+            scl_pin=self.init_pin(15, "Display SCL"),
+            sda_pin=self.init_pin(4, "Display SDA"),
         )
         self.init_ds18x20(
-            ow_pin=Pin(0, Pin.OUT)
+            ow_pin=self.init_pin(0, "Temperature 1-Wire", Pin.OUT)
         )
-        # TODO: right now, actuators are dummys
-        self.actuators = {
-            "1_1": Actuator(Pin(25, Pin.OUT)),
-            "1_3": Actuator(Pin(12, Pin.OUT)),
-            "1_5": Actuator(Pin(13, Pin.OUT)),
-            "2_1": Actuator(Pin(17, Pin.OUT)),
-            "2_3": Actuator(Pin( 2, Pin.OUT)),
-            "2_5": Actuator(Pin(23, Pin.OUT)),
-            "2_7": Actuator(Pin(22, Pin.OUT)),
+        mapping = {
+            "1_1": 25,
+            "1_3": 12,
+            "1_5": 13,
+            "2_1": 17,
+            "2_3":  2,
+            "2_5": 23,
+            "2_7": 22,
         }
+        for name, num in mapping.items():
+            self.init_pin(num, name, Pin.OUT)
