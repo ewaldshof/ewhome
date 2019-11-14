ed3bc1e86fdf0e3275f3e1e1e5e8f97427668ec5
Tim Weber <mail@timweber.name>
2019-11-14 19:14:44 +0100

    Add output_pins part

---
+++ firmware/parts/output_pins.py
@@ -0,0 +1,24 @@
+from parts import Part
+
+# Example configuration:
+# output_pins:
+#   1-1: A1.2/allow_fan and A1.2/temperature > 25
+#   1-3: A1.2/ceiling_light
+
+class OutputPins(Part):
+
+    def boot(self):
+        self.handlers = {}
+        for pin_name, expression in self.config.items():
+            if pin_name in self.handlers:
+                raise RuntimeError("cannot associate expression {0} to pin {1}, pin already in use".format(expression, pin_name))
+            self.handlers[pin_name] = OutputPinHandler(self.mqtt, self.board.get_pin(pin_name), expression)
+
+class OutputPinHandler:
+
+    def __init__(self, mqtt, pin, expression):
+        self.pin = pin
+        self.expression = mqtt.subscribe_expression(expression, self._on_change)
+
+    def _on_change(self, expression, value):
+        self.pin(bool(value))
