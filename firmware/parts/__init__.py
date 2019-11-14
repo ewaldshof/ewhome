7e8e6f48a2ebc6d1dd0ea34f952f615529d59e78
Tim Weber <mail@timweber.name>
2019-11-14 15:17:29 +0100

    Easier access to mqtt and board for parts

--- firmware/parts/__init__.py
+++ firmware/parts/__init__.py
@@ -3,6 +3,8 @@ class Part:
     def __init__(self, config, services):
         self.config = config
         self.services = services
+        self.board = services.board
+        self.mqtt = services.mqtt
 
     def boot(self):
         pass
