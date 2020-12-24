fcc69cfd29c330ea7c17273e42d76b61ca90d60c
sulimma <kolja@sulimma.de>
2020-12-24 22:58:12 +0100

    moved part initializsation from config.py to class Part

--- firmware/config.py
+++ firmware/config.py
@@ -14,9 +14,6 @@ cachedir = "/cache"
 cachefile = cachedir + "/config.json"
 
 class Config:
-    @staticmethod
-    def print_exception(e, msg="Exception in Config:"):
-        print( "{0}: {1}: {2}".format(msg, type(e).__name__, str(e)))
 
 
     def __init__(self, board, network, mqtt, scheduler):
@@ -30,39 +27,10 @@ class Config:
         self.version = "0.7.0"
         self.listeners = [self.board.temperature._on_config_update] # TODO: Hack until Temperature becomes a Part.
         self.read_cache()
-        self.init_parts()
+        if (type(self.mine) is dict and "parts" in self.mine):
+            Part.init_parts(board, mqtt, scheduler, self.mine["parts"])
         mqtt.subscribe("config", self.on_mqtt)
 
-    def init_parts(self):
-        if self.parts_initialized:
-            return
-        self.parts_initialized = True
-        if not (type(self.mine) is dict and "parts" in self.mine):
-            return
-        Part.setup_services(self.board, self.mqtt, self.scheduler)
-        for partname, partconfig in self.mine["parts"].items():
-            modname = "parts." + partname
-            classname = "".join(word[0].upper() + word[1:] for word in partname.split("_"))
-            #output heading for each part type in blue
-            print("\033[94mInitializing part: {0} (import {1} from {2})\x1b[0m".format(partname, classname, modname))
-            instance = None
-            try:
-                imported = __import__("parts." + partname, globals(), locals(), [classname])
-            except Exception as e:
-                Config.print_exception("Import failed", e)
-                continue
-
-            try:
-                cls = getattr(imported, classname)
-            except Exception as e:
-                Config.print_exception("getattr failed:", e)
-                continue
-
-            try:
-                cls.boot(partconfig)
-            except Exception as e:
-                Config.print_exception("Class boot failed:", e)
-                continue
  
     def read_cache(self):
         data = None
--- firmware/parts/__init__.py
+++ firmware/parts/__init__.py
@@ -15,4 +15,40 @@ class Part:
         for key, content in config.items():
             cls.instances[key] = cls(key, content)
 
-    
+    @staticmethod
+    def print_exception(e, msg="Exception in Config:"):
+        print( "\033[91m{0}: {1}: {2}\x1b[0m".format(msg, type(e).__name__, str(e)))
+
+    initialized = False
+
+    @classmethod
+    def init_parts(cls, board, mqtt, scheduler, part_config):
+        if Part.initialized:
+            return
+        Part.initialized = True
+        Part.board = board
+        Part.mqtt = mqtt
+        Part.scheduler = scheduler
+
+        for partname, partconfig in part_config.items():
+            modname = "parts." + partname
+            classname = "".join(word[0].upper() + word[1:] for word in partname.split("_"))
+            #output heading for each part type in blue
+            print("\033[94mInitializing part: {0} (import {1} from {2})\x1b[0m".format(partname, classname, modname))
+            try:
+                imported = __import__("parts." + partname, globals(), locals(), [classname])
+            except Exception as e:
+                Config.print_exception("Import failed", e)
+                continue
+
+            try:
+                cls = getattr(imported, classname)
+            except Exception as e:
+                Config.print_exception("getattr failed:", e)
+                continue
+
+            try:
+                cls.boot(partconfig)
+            except Exception as e:
+                Config.print_exception("Class boot failed:", e)
+                continue
